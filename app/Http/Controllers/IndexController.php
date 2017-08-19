f93ed8cbdf774ea9fab57f372f93ebd0e565fcea
lublank <lublank@126.com>
2017-08-19 18:50:53 +0800

    IndexController

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
+    // index 页面
     public function getIndexPage() {
         return view('index', [
             'page_title' => 'Welcome to Laravel, Hell world!',
